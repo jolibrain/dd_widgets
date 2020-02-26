bd1f11207a4314c0d88e5f1ca577664a53305ca9
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2020-02-26 09:39:03 +0100

    fix option form geomtric deformation of image in detection problems

--- dd_widgets/mixins.py
+++ dd_widgets/mixins.py
@@ -341,7 +341,7 @@ class ImageTrainerMixin(MLWidget):
             }
         if any(
              [
-                 #self.all_effects.value,
+                 self.all_effects.value,
                  self.persp_horizontal.value,
                  self.persp_vertical.value,
                  self.zoom_out.value,
@@ -358,8 +358,8 @@ class ImageTrainerMixin(MLWidget):
          ):
              dic["geometry"] = {}
              # -- booleans --
-             #if self.all_effects.value:
-             #    dic["geometry"]["all_effects"] = True
+             if self.all_effects.value:
+                 dic["geometry"]["all_effects"] = True
              if self.persp_horizontal.value:
                  dic["geometry"]["persp_horizontal"] = True
              if self.persp_vertical.value:
