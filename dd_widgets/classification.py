a05709810c599f426d7026663060e8fd907ea94b
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2021-04-20 14:24:28 +0200

    feat: histogram equalization

--- dd_widgets/classification.py
+++ dd_widgets/classification.py
@@ -67,6 +67,7 @@ class Classification(ImageTrainerMixin):
         finetune: bool = False,
         resume: bool = False,
         bw: bool = False,
+        histogram_equalization: bool = False,
         crop_size: int = -1,
         batch_size: int = 32,
         test_batch_size: int = 16,
