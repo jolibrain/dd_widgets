e3af81def4856bb5885c4fe475ba9aa4c3fd6e51
Louis Jean <lsj456@gmail.com>
2022-04-26 14:41:36 +0200

    fix: add rgb option to OCR

--- dd_widgets/ocr.py
+++ dd_widgets/ocr.py
@@ -67,6 +67,7 @@ class OCR(ImageTrainerMixin):
         finetune: bool = False,
         resume: bool = False,
         bw: bool = False,
+        rgb: bool = False,
         histogram_equalization: bool = False,
         crop_size: int = -1,
         batch_size: int = 32,
