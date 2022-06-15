d56dd388c88f6f4d9c11824f97a41a4d747e5372
Louis Jean <lsj456@gmail.com>
2022-06-15 19:13:07 +0200

    feat(ocr): add timesteps to create_mllib call

--- dd_widgets/ocr.py
+++ dd_widgets/ocr.py
@@ -143,6 +143,11 @@ class OCR(ImageTrainerMixin):
                 for fh in sample_from_iterable(self.file_dict.keys(), 10)
             ]
 
+    def _create_parameters_mllib(self) -> JSONType:
+        dic = super()._create_parameters_mllib()
+        dic["timesteps"] = self.timesteps.value
+        return dic
+
     def _train_parameters_mllib(self) -> JSONType:
         dic = super()._train_parameters_mllib()
         dic["timesteps"] = self.timesteps.value
